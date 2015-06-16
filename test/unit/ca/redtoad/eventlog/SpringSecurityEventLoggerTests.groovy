857b74a7fb1471a521e438f836f1518ec0228ed2
hitokun-s <jgpuauno+github@gmail.com>
2015-06-16 16:35:04 +0900

    x-forwarded-forヘッダ取得のテストを追加

--- test/unit/ca/redtoad/eventlog/SpringSecurityEventLoggerTests.groovy
+++ test/unit/ca/redtoad/eventlog/SpringSecurityEventLoggerTests.groovy
@@ -1,14 +1,24 @@
 package ca.redtoad.eventlog
 
+import grails.test.mixin.Mock
+import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletRequest
+import org.junit.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.security.authentication.TestingAuthenticationToken
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.authentication.switchuser.AuthenticationSwitchUserEvent
+
+import javax.servlet.http.HttpServletRequest
 
 @Mock([SpringSecurityEvent])
 class SpringSecurityEventLoggerTests {
 
     def logger = new SpringSecurityEventLogger()
 
+    @Test
     void testLogAuthenticationEventWithNullAuthentication() {
         logger.logAuthenticationEvent("event", null, "127.0.0.1", null)
 
@@ -21,6 +31,7 @@ class SpringSecurityEventLoggerTests {
         assert event.remoteAddress == "127.0.0.1"
     }
 
+    @Test
     void testLogAuthenticationEventWithStringPrincipal() {
         def authentication = new TestingAuthenticationToken("username", [])
         logger.logAuthenticationEvent("event", authentication, "127.0.0.1", null)
@@ -34,6 +45,7 @@ class SpringSecurityEventLoggerTests {
         assert event.remoteAddress == "127.0.0.1"
     }
 
+    @Test
     void testLogAuthenticationEventWithUserDetailsPrincipal() {
         def principal = { -> "username" } as UserDetails
         def authentication = new TestingAuthenticationToken(principal, [])
@@ -48,6 +60,7 @@ class SpringSecurityEventLoggerTests {
         assert event.remoteAddress == "127.0.0.1"
     }
 
+    @Test
     void testLogAuthenticationSwitchUserEvent() {
         def principal = { -> "username" } as UserDetails
         def authentication = new TestingAuthenticationToken(principal, [])
@@ -63,6 +76,80 @@ class SpringSecurityEventLoggerTests {
         assert event.eventName == "AuthenticationSwitchUserEvent"
         assert event.switchedUsername == "switchedUsername"
         assert event.remoteAddress == "127.0.0.1"
+    }
+
+    @Test
+    void testGetRemoteAddrFromXForwardedFor(){
+
+        def principal = new UserDetails() {
+
+            public Integer id = 1
+
+            @Override
+            Collection<GrantedAuthority> getAuthorities() {
+                return null
+            }
+            @Override
+            String getPassword() {
+                return "test password"
+            }
+            @Override
+            String getUsername() {
+                return "test user"
+            }
+            @Override
+            boolean isAccountNonExpired() {
+                return true
+            }
+            @Override
+            boolean isAccountNonLocked() {
+                return true
+            }
+            @Override
+            boolean isCredentialsNonExpired() {
+                return true
+            }
+            @Override
+            boolean isEnabled() {
+                return true
+            }
+        }
+        def authentication = new TestingAuthenticationToken(principal, [])
+        authentication.setDetails(remoteAddress : "192.168.1.0")
+
+        // test for default remote address (without x-forwarded-for header)
+        MockHttpServletRequest request0 = new MockHttpServletRequest();
+        logger.request = request0
+        logger.onApplicationEvent(new AuthenticationSuccessEvent(authentication))
+        assert SpringSecurityEvent.list()[0].remoteAddress == "192.168.1.0"
+
+        // test for "x-forwarded-for"
+        MockHttpServletRequest request1 = new MockHttpServletRequest();
+        request1.addHeader("x-forwarded-for", "192.168.1.1")
+        logger.request = request1
+        logger.onApplicationEvent(new AuthenticationSuccessEvent(authentication))
+        assert SpringSecurityEvent.list()[1].remoteAddress == "192.168.1.1"
+
+        // test for "x_forwarded_for"
+        MockHttpServletRequest request2 = new MockHttpServletRequest();
+        request2.addHeader("x_forwarded_for", "192.168.1.2")
+        logger.request = request2
+        logger.onApplicationEvent(new AuthenticationSuccessEvent(authentication))
+        assert SpringSecurityEvent.list()[2].remoteAddress == "192.168.1.2"
+
+        // test for "X-Forwarded-For"
+        MockHttpServletRequest request3 = new MockHttpServletRequest();
+        request3.addHeader("X-Forwarded-For", "192.168.1.3")
+        logger.request = request3
+        logger.onApplicationEvent(new AuthenticationSuccessEvent(authentication))
+        assert SpringSecurityEvent.list()[3].remoteAddress == "192.168.1.3"
+
+        // test for "X_Forwarded_For"
+        MockHttpServletRequest request4 = new MockHttpServletRequest();
+        request4.addHeader("X_Forwarded_For", "192.168.1.4")
+        logger.request = request4
+        logger.onApplicationEvent(new AuthenticationSuccessEvent(authentication))
+        assert SpringSecurityEvent.list()[4].remoteAddress == "192.168.1.4"
     }
 
 }
